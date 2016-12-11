65c4fc85fedb79502e59317379fe51c1babf30f9
Guayo Mena <guayo.mena@gmail.com>
2016-12-11 15:58:53 -0600

    Started creating structure for flowers

--- config.rb
+++ config.rb
@@ -1,20 +1,28 @@
 ###
 # Page options, layouts, aliases and proxies
 ###
-
-# Per-page layout changes:
-#
 # With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+#Pages for families
+data.families.each do |family|
+  proxy "/en/#{family.nombre_en.parameterize}/index.html", "/family-template.html",
+    :locals => {
+      :family => family,
+      :family_name => family.nombre_en,
+      :family_description => family.characteristics_en,
+      :language => "en"
+      }, :ignore => true
+  proxy "/es/#{family.nombre_es.parameterize}/index.html", "/family-template.html",
+    :locals => {
+      :family => family,
+      :family_name => family.nombre_es,
+      :family_description => family.characteristics_es,
+      :language => "es"
+      }, :ignore => true
+end
 
 # General configuration
 page "/backgroud.html", :layout => "content"
